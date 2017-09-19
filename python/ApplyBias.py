05597c20e13a9104a0f9aa5f9d880a3d27515474
MorganSchmitz <morgan.schmitz@cea.fr>
2017-09-19 15:08:05 +0200

    Add python script to apply bias to galaxy catalog

---
+++ python/ApplyBias.py
@@ -0,0 +1,79 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# set path to Camelus outputs
+data_path = '../build/'
+
+def ComputeDensity(galcat):
+    """Compute density delta(x,y) from a Camelus-generated galaxy catalog.
+
+        Parameters
+        ----------
+        galcat : np.ndarray
+            Camelus-generated galaxy catalog as numpy array.
+
+        Returns
+        -------
+        delta : tuple
+            Density in numpy.histogram2d convention, that is, delta[0][i,j] is the 
+            density for galaxies at position x=delta[1][i], y=delta[2][j].
+    """
+    x_bins = range(int(np.min(galcat[:,0])),int(np.max(galcat[:,0]))+1)
+    y_bins = range(int(np.min(galcat[:,1])),int(np.max(galcat[:,1]))+1)
+    delta = np.histogram2d(galcat[:,0], galcat[:,1], bins=[x_bins,y_bins])
+    return delta
+
+def ApplyBias(galcat, delta, a=-0.00018213, b= -0.03544):
+    """Compute and apply multiplicative bias to shear.
+
+        Parameters
+        ----------
+        galcat : np.ndarray
+            Camelus-generated galaxy catalog as numpy array.
+            
+        delta : tuple
+            Density in numpy.histogram2d convention.
+            
+        a : float
+            Slope for the linear relationship between bias and density.
+            Default: value from Hoekstra et al., 2016, Figure 5/Section 5.2.
+            
+        b : float
+            Intercept for the linear relationship between bias and density.
+            Default: value from Hoekstra et al., 2016, Figure 5/Section 5.2.
+        
+        Returns
+        -------
+        galcat_b : np.ndarray
+            Galaxy catalog with multiplicative bias applied to shear measurements.
+    """
+    galcat_b = np.copy(galcat)
+    xs, ys = galcat[:,0].astype(int), galcat[:,1].astype(int)
+    # add maximum position to last bin
+    xs[xs == np.max(delta[1])] = np.max(delta[1])-1
+    ys[ys == np.max(delta[2])] = np.max(delta[2])-1
+    delta_pos = np.array([delta[0][np.where(delta[1]==x)[0],np.where(delta[2]==y)[0]] 
+                            for x,y in zip(xs,ys)])
+    m = a + b*delta_pos
+    galcat_b[:,-2:] *= (1+m)
+    return galcat_b
+    
+def main():
+    galcat = np.loadtxt(data_path+'galCat')
+    # compute density from galaxy catalog
+    delta = ComputeDensity(galcat)
+    # plot density map  
+    plt.imshow(delta[0], interpolation='Nearest', cmap='viridis')
+    plt.colorbar()
+    plt.xlabel(r'$\theta_x$ (arcmin)')
+    plt.ylabel(r'$\theta_y$ (arcmin)')
+    plt.title(r'Density map')
+    plt.show()
+    plt.close()
+    # apply bias
+    galcat_b = ApplyBias(galcat, delta)
+    # save biased galaxy catalog
+    np.savetxt(data_path+'galCat_b', galcat_b)
+    
+if __name__ == "__main__":
+    main()
