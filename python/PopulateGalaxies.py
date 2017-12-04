9dbc4d5df66e98385dccc282bb4702cb895b960b
theodore <theodore@client-172-18-76-185.clients.u-psud.fr>
2017-12-04 15:31:51 +0100

    Populategalaxies2

--- python/PopulateGalaxies.py
+++ python/PopulateGalaxies.py
@@ -6,17 +6,24 @@ import matplotlib.pyplot as plt
 
 def PopulateGalaxiesFromHalo(filename):
     halocat = np.genfromtxt(filename, names=True, skip_header = 10)
-    gal = np.array()
+    First = True
     for halo in halocat :
-        np.vstack((gal,PopulateOneHalo(halo)))
+        if First :
+            gal = PopulateOneHalo(halo)
+            First = False
+        else :
+            gal = np.vstack((gal,PopulateOneHalo(halo)))
+            
 
     return gal
 
 def PopulateOneHalo(halo) :
-    gal = np.array()
-    if np.random.random() < halo['Ngal_c'] :
-        gal.append([halo['theta_x'], halo['theta_y'], halo['z']])
 
+    First = True
+    if np.random.random() < halo['Ngal_c'] :
+        gal = np.array([halo['theta_x'], halo['theta_y'], halo['z']])
+        First = False
+        
     for i in xrange(np.int(halo['Ngal_s']+0.5)) :
         R_false = True
         while R_false :
@@ -28,13 +35,16 @@ def PopulateOneHalo(halo) :
         phi = np.arccos(np.random.random() * 2 - 1)
         x = np.cos(theta) * np.sin(phi) * r
         y = np.sin(theta) * np.sin(phi) * r
-        gal.append([halo['theta_x'] + x, halo['theta_y'] + y, halo['z']])
+        if First :
+            gal = np.array([halo['theta_x'] + x, halo['theta_y'] + y, halo['z']])
+        else :
+            gal = np.vstack((gal,[halo['theta_x'] + x, halo['theta_y'] + y, halo['z']]))
 
     return gal
 
 
 def NFW(x) :
-    return 1/(x*(1+np.pow(x,2)))
+    return 1/(x*(1+np.power(x,2)))
     
 
 
@@ -90,7 +100,21 @@ def main():
     plt.legend()
     #plt.show()
     plt.savefig(cats_dir+'Nz_plot.png')
+
+
+def main():
+
+    cats_dir, halcat_name, galcat_name = sys.argv[1], sys.argv[2], sys.argv[3]
+    stub_length = len(halcat_name)
+    halcat_files = [catfile for catfile in os.listdir(cats_dir) 
+                    if halcat_name == catfile[:stub_length]]
+    for halcat in halcat_files :
+        galcat = PopulateGalaxiesFromHalo(halcat)
+        idnb = '_'
+        for digit in [char for char in halcat[-3:] if char.isdigit()]:
+            idnb += digit
+            np.savetxt(cats_dir + galcat_name + idnb, galcat)
     
     
 if __name__ == "__main__":
-    main()
+   main()
