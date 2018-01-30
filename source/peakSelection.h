03a86c3f5f273de1b67962c56591d52587a9e9db
theodore <theodore@MacBook-Pro-de-admin.local>
2018-01-30 15:04:17 +0100

    complete peakSelection.h

--- source/peakSelection.h
+++ source/peakSelection.h
@@ -60,6 +60,8 @@ void doPeakList_withInputs(char fileName[], char fileName2[],char opt[],cosmo_hm
 void doProduce_Catalog_N(int N,char HaloFileName[],char GalFileName[], cosmo_hm *cmhm, peak_param *peak, error **err);
 void doPeakList_withInputs_N(int N,char fileName[], char fileName2[],char end[],cosmo_hm *cmhm, peak_param *peak, error **err);
 void doProduce_Catalog_DM_HOD(int N,char CmhmName[],char HaloFileName[], cosmo_hm *cmhm, peak_param *peak, error **err);
+void doPeakList_withInputs_hod(char fileNameHal[], char fileNameGal[],char end[],cosmo_hm *cmhm, peak_param *peak, error **err);
+void doProduce_Catalog_DM_galaxies(int N, char CmhmName[], char HaloFileName[], char GalaxyFileName[], cosmo_hm *cmhm, peak_param *peak, error **err);
 
 #endif
 
